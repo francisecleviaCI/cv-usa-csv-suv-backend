ab325dbfd4f4d95c960c88a7378ba22d36694af2
Francis Eclevia <65243967+francisecleviaCI@users.noreply.github.com>
2020-07-16 09:58:04 -0400

    update

--- get-rows.js
+++ get-rows.js
@@ -1,5 +1,21 @@
+const getRow = require('./get-row.js');
 
 
+const newArray = []; //Make a new array
+const getRows = function(csv){
+
+const newRows = getRow(csv);  //Split the csv string //getRow()
+
+//loop through the array
+for (let i = 0; i < newRows.length; i++){
+
+//push an array for each string into new array
+
+newArray.push(newRows)
+}
+return newArray;
+}
+console.log(newArray)
 
 
 if (typeof getRows === 'undefined') {
