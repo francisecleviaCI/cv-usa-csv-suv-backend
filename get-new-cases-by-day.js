cb1b8b7eb9e498df42b894a593636c759710100e
Francis Eclevia <65243967+francisecleviaCI@users.noreply.github.com>
2020-07-15 23:30:35 -0400

    update

--- get-new-cases-by-day.js
+++ get-new-cases-by-day.js
@@ -1,4 +1,25 @@
+const getDate = require('./get-date.js');
+const getNewCases = require('./get-new-cases.js');
 
+//takes in a date string & 2D array//
+//date is 3rd Index, new cases is 5th index//
+const getNewCasesByDay = function(dateString, array){
+
+
+
+
+//loop through each row of the data//is this nested
+for (let i = 0; i < dateString.length; i++){
+  for (let j = 0; j < array.length; j++){
+const theDate = getDate(dateString[i]);
+const caseArray = getNewCases(theDate);
+
+//returns total cases
+return caseArray;
+}
+}
+console.log(caseArray);
+}
 
 
 
--- get-total-cases-by-day.js
+++ get-total-cases-by-day.js
@@ -1,3 +1,25 @@
+const getDate = require('./get-date.js');
+const getTotalCases = require('./get-total-cases.js');
+
+//takes in a date string & 2D array//
+//date is 3rd Index, cases is 4th index//
+const getTotalCasesByDay = function(dateString, array){
+
+
+
+
+//loop through each row of the data//is this nested?
+for (let i = 0; i < dateString.length; i++){
+  for (let j = 0; j < array.length; j++){
+const theDate = getDate(dateString[i]);
+const caseArray = getTotalCases(theDate);
+
+//returns total cases
+return caseArray;
+}
+}
+console.log(caseArray);
+}
 
 
 
